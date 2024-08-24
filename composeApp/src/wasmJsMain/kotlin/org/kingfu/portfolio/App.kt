a94b1ffbde015d11aeecb7ecc20b52658174c268
longnguyen <kingfuweatherai@gmail.com>
2024-08-24 01:15:38 -0700

    3rd commit

--- composeApp/src/wasmJsMain/kotlin/org/kingfu/portfolio/App.kt
+++ composeApp/src/wasmJsMain/kotlin/org/kingfu/portfolio/App.kt
@@ -2,11 +2,17 @@ package org.kingfu.portfolio
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.MaterialTheme
-import androidx.compose.runtime.*
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import org.jetbrains.compose.resources.painterResource
 import portfolio.composeapp.generated.resources.Res
 import portfolio.composeapp.generated.resources.longnguyen
@@ -14,8 +20,22 @@ import portfolio.composeapp.generated.resources.longnguyen
 @Composable
 fun App() {
     MaterialTheme {
-        Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-            Image(painterResource(Res.drawable.longnguyen), null)
+        val scrollState = rememberScrollState()
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .verticalScroll(state = scrollState),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                modifier = Modifier.height(height = 400.dp),
+                painter = painterResource(Res.drawable.longnguyen),
+                contentDescription = null
+            )
+
+            Spacer(modifier = Modifier.height(height = 18.dp))
+
+            Text(text = "Hi, my name is Long Nguyen. I am a Software Engineer.")
         }
     }
 }
