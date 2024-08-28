585acc1ba82dcd0cd5a35aceff3a0f0c755f5785
longnguyen <kingfuweatherai@gmail.com>
2024-08-28 00:36:43 -0700

    10 commit

--- composeApp/src/wasmJsMain/kotlin/org/kingfu/portfolio/home/HomeScreen.kt
+++ composeApp/src/wasmJsMain/kotlin/org/kingfu/portfolio/home/HomeScreen.kt
@@ -1,39 +1,27 @@
 package org.kingfu.portfolio.home
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.IntrinsicSize
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme.colorScheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.scale
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import org.kingfu.portfolio.ui.theme.Shape
 import com.kingfu.imaginate.ui.theme.TextBodyLarge
-import org.jetbrains.compose.resources.painterResource
+import org.kingfu.portfolio.home.component.Imaginate
+import org.kingfu.portfolio.home.component.Introduction
 import org.kingfu.portfolio.navigation.Screen
 import org.kingfu.portfolio.topBar.MenuTopBar
-import portfolio.composeapp.generated.resources.Res
-import portfolio.composeapp.generated.resources.longnguyen
 
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -62,80 +50,27 @@ fun HomeScreen(
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
 
-            BoxWithConstraints(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(horizontal = 16.dp),
-                propagateMinConstraints = true
-            ) {
-                val maxWidth = this.maxWidth
-                val text = "Hi, my name is Long Nguyen. I am a Software Engineer based in Anaheim, California"
-                val fontSize = 36.sp
-                val lineHeight = 48.sp
-                val shape = Shape.medium
-                val scale = 0.7f
+            Introduction()
 
-                if (maxWidth <= 700.dp) {
-                    Column(
-                        modifier = Modifier
-                            .height(intrinsicSize = IntrinsicSize.Max)
-                            .fillMaxWidth()
-                    ) {
-                        Image(
-                            modifier = Modifier
-                                .scale(scale  = scale)
-                                .clip(shape = shape)
-                            ,
-                            painter = painterResource(Res.drawable.longnguyen),
-                            contentDescription = null
-                        )
+            Spacer(modifier = Modifier.height(height = 128.dp))
 
-                        TextBodyLarge(
-                            modifier = Modifier
-                                .fillMaxWidth(),
-                            text = text,
-                            textAlign = TextAlign.Center,
-                            fontSize = fontSize,
-                            lineHeight = lineHeight
-                        )
-                    }
-                } else {
-                    Row(
-                        modifier = Modifier
-                            .fillMaxWidth(),
-                        horizontalArrangement = Arrangement.SpaceEvenly,
-                        verticalAlignment = Alignment.CenterVertically
-                    ) {
-                        Image(
-                            modifier = Modifier
-                                .scale(scale  = scale)
-                                .weight(weight = 0.5f)
-                                .clip(shape = shape)
-                            ,
-                            painter = painterResource(Res.drawable.longnguyen),
-                            contentDescription = null
-                        )
+            val fontSize = 42.sp
+            val lineHeight = 52.sp
 
-                        Spacer(modifier = Modifier.width(width = 16.dp))
+            TextBodyLarge(
+                modifier = Modifier.fillMaxWidth(),
+                text = "Latest Work",
+                fontSize = fontSize,
+                lineHeight = lineHeight,
+                textAlign = TextAlign.Center
+            )
 
-                        TextBodyLarge(
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .weight(weight = 0.5f),
-                            text = text,
-                            textAlign = TextAlign.Center,
-                            fontSize = fontSize,
-                            lineHeight = lineHeight
-                        )
-                    }
-                }
-            }
+            Spacer(modifier = Modifier.height(height = 32.dp))
 
+            Imaginate()
 
-            Spacer(modifier = Modifier.height(height = 1500.dp))
+            Spacer(modifier = Modifier.height(height = 500.dp))
         }
-
-
     }
 }
 
---
+++ composeApp/src/wasmJsMain/kotlin/org/kingfu/portfolio/home/component/Imaginate.kt
@@ -0,0 +1,209 @@
+package org.kingfu.portfolio.home.component
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.platform.LocalUriHandler
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.kingfu.imaginate.ui.theme.TextBodyLarge
+import org.jetbrains.compose.resources.painterResource
+import org.kingfu.portfolio.ui.theme.Shape
+import portfolio.composeapp.generated.resources.Res
+import portfolio.composeapp.generated.resources.imaginate
+import kotlin.math.log10
+
+@Composable
+fun Imaginate(modifier: Modifier = Modifier) {
+
+    Column(
+        modifier = modifier.fillMaxWidth()
+    ) {
+//        val fontSize = 42.sp
+//        val lineHeight = 52.sp
+//
+//        TextBodyLarge(
+//            modifier = Modifier.fillMaxWidth(),
+//            text = "Latest Work",
+//            fontSize = fontSize,
+//            lineHeight = lineHeight,
+//            textAlign = TextAlign.Center
+//        )
+//
+//        Spacer(modifier = Modifier.height(height = 32.dp))
+
+        BoxWithConstraints(
+            modifier = modifier
+                .fillMaxSize()
+                .padding(horizontal = 16.dp),
+            propagateMinConstraints = true
+        ) {
+            val maxWidth = this.maxWidth
+            val title = "Imaginate"
+            val scaleMultiplier =
+                log10((maxWidth.value + 10).coerceIn(1f, 1000f)) * 0.25f // Adjust 0.2f as needed
+            val titleFontSize = 42.sp * scaleMultiplier
+            val titleLineHeight = 52.sp * scaleMultiplier
+            val subTitle = "Android Application"
+            val subTitleFontSize = 24.sp * scaleMultiplier
+            val subTitleLineHeight = 34.sp * scaleMultiplier
+            val body =
+                "Imaginate, where creative innovation comes to life. Explore AI-generated masterpieces, " +
+                        "download them, or set as wallpapers. Enjoy interactive features like zoom and pan, and " +
+                        "discover new art through intuitive search. Customize your experience with themes, and stay tuned for exciting updates."
+            val bodyFontSize = 24.sp * scaleMultiplier
+            val bodyLineHeight = 34.sp * scaleMultiplier
+            val shape = Shape.medium
+            val scale = 1f
+            val resource = Res.drawable.imaginate
+            val fontWeight = FontWeight.Light
+            val download = "Download"
+            val downloadFontSize = 16.sp
+            val downloadLineHeight = 26.sp
+            val uriHandler = LocalUriHandler.current
+            val imaginateUrl = "https://play.google.com/store/apps/details?id=com.kingfu.aigallery&hl=en_US"
+
+
+            if (maxWidth <= 700.dp) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    verticalArrangement = Arrangement.spacedBy(space = 24.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Image(
+                        modifier = Modifier
+                            .scale(scale = scale)
+                            .clip(shape = shape),
+                        painter = painterResource(resource = resource),
+                        contentDescription = null
+                    )
+
+                    Column(modifier = Modifier.fillMaxWidth()) {
+                        TextBodyLarge(
+                            text = title,
+                            fontSize = titleFontSize,
+                            lineHeight = titleLineHeight,
+                        )
+
+                        Spacer(modifier = Modifier.height(height = 8.dp))
+
+                        TextBodyLarge(
+                            text = subTitle,
+                            fontSize = subTitleFontSize,
+                            lineHeight = subTitleLineHeight,
+                            fontWeight = fontWeight
+                        )
+
+                        Spacer(modifier = Modifier.height(height = 32.dp))
+
+                        TextBodyLarge(
+                            text = body,
+                            fontSize = bodyFontSize,
+                            lineHeight = bodyLineHeight,
+                            fontWeight = fontWeight
+                        )
+
+                        Spacer(modifier = Modifier.height(height = 42.dp))
+
+                        OutlinedButton(
+                            onClick = { uriHandler.openUri(uri = imaginateUrl) }
+                        ) {
+                            TextBodyLarge(
+                                text = download,
+                                fontSize = downloadFontSize,
+                                lineHeight = downloadLineHeight,
+                                fontWeight = fontWeight
+                            )
+                        }
+
+                    }
+                }
+            } else {
+                Row(
+                    modifier = Modifier
+                        .padding(horizontal = 16.dp)
+                        .fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceEvenly,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Box(
+                        modifier = Modifier.weight(weight = 0.5f),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Image(
+                            modifier = Modifier
+                                .scale(scale = scale)
+                                .clip(shape = shape),
+                            painter = painterResource(resource = resource),
+                            contentDescription = null
+                        )
+                    }
+
+                    Spacer(modifier = Modifier.width(width = 16.dp))
+
+                    Column(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .weight(weight = 0.5f)
+                    ) {
+                        TextBodyLarge(
+                            text = title,
+                            fontSize = titleFontSize,
+                            lineHeight = titleLineHeight,
+                        )
+
+                        Spacer(modifier = Modifier.height(height = 8.dp))
+
+                        TextBodyLarge(
+                            text = subTitle,
+                            fontSize = subTitleFontSize,
+                            lineHeight = subTitleLineHeight,
+                            fontWeight = fontWeight
+                        )
+
+                        Spacer(modifier = Modifier.height(height = 32.dp))
+
+                        TextBodyLarge(
+                            text = body,
+                            fontSize = bodyFontSize,
+                            lineHeight = bodyLineHeight,
+                            fontWeight = fontWeight
+                        )
+
+                        Spacer(modifier = Modifier.height(height = 42.dp))
+
+                        OutlinedButton(
+                            onClick = { uriHandler.openUri(uri = imaginateUrl) }
+                        ) {
+                            TextBodyLarge(
+                                text = download,
+                                fontSize = downloadFontSize,
+                                lineHeight = downloadLineHeight,
+                                fontWeight = fontWeight
+                            )
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+
+}
