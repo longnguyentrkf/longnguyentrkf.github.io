7d6bb2014666784c1ad7c3d99f51157d2a7493df
longnguyen <kingfuweatherai@gmail.com>
2024-08-30 00:36:29 -0700

    15th commit

--- composeApp/src/wasmJsMain/kotlin/org/kingfu/portfolio/navigation/AppNavHost.kt
+++ composeApp/src/wasmJsMain/kotlin/org/kingfu/portfolio/navigation/AppNavHost.kt
@@ -1,6 +1,5 @@
 package org.kingfu.portfolio.navigation
 
-import androidx.compose.animation.AnimatedContentTransitionScope
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
@@ -15,26 +14,12 @@ fun AppNavHost(
     homeViewModel: HomeViewModel
 ) {
     val navController = rememberNavController()
-    val enterTransition = AnimatedContentTransitionScope.SlideDirection.Left
-    val popTransition = AnimatedContentTransitionScope.SlideDirection.Right
 
 
     NavHost(
         modifier = modifier,
         navController = navController,
-        startDestination = Screen.Home.route,
-        enterTransition = {
-            slideIntoContainer(towards = enterTransition)
-        },
-        exitTransition = {
-            slideOutOfContainer(towards = enterTransition)
-        },
-        popEnterTransition = {
-            slideIntoContainer(towards = popTransition)
-        },
-        popExitTransition = {
-            slideOutOfContainer(towards = popTransition)
-        }
+        startDestination = Screen.Home.route
     ) {
         homeGraph(
             vm = homeViewModel
